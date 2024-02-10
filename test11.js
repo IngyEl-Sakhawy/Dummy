5b50111c6f3f0ff06d3036531952a57409243fb1
ingy <ingy.mahmoud.elsakhawy@gmail.com>
2024-02-10 19:41:44 +0200

    Dummies

---
+++ test11.js
@@ -0,0 +1,67 @@
+// let titleName=" el zero ",titleDescription="elzero web school",titleDate="25/10";
+// let markUp=`
+// <div class="card">
+// <h2>${titleName}</h2>
+// <p>${titleDescription}</p>
+// <span>${titleDate}</span>
+// </div>
+// `; 
+
+// document.write(markUp);
+
+
+// let a = 1_00;
+// let b = 2_00.5;
+// let c = 1e2;
+// let d = 2.4;
+
+// console.log(Math.min(a,b,c,d));
+// console.log(Math.floor(d));
+// console.log(Math.round(d));
+// console.log(parseInt(d));
+// console.log(d.toFixed(2));
+// let namef ="  ahmed  ";
+// console.log(namef.charAt(4).toUpperCase());
+
+
+// let my  = ["Ahmed",  "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+
+
+// console.log(my.slice(0,4).reverse()); //  ["Osama",  "Elham",  "Mazero",  "Ahmed"]
+
+// console.log(my.slice(1,3).reverse()); // ["Elham", . "Mazero"]
+// my[1]=["elzero"];
+// console.log(my[1]); //"Elzero"
+
+// console. log(); //"rO"
+
+
+
+// let myAdmins=["ahmed","osama","sayed","stop","samera"];
+// let myEmp=["amged","sameh","ameer","omar","othman","amany","samia"];
+// let i=0;
+// let j=0;
+// document.write(`<div>we have ${myAdmins.length} admins</div>`);
+
+// while(true){
+//     if(myAdmins[i]=="stop"){
+//     document.write(`<div>we have ${i} admins</div>`);
+//     break;
+//     }
+// i++;
+// }
+
+// while (j<myAdmins.length){
+
+//     document.write(`<h1>the admin of team ${j+1} is ${myAdmins[j]} </h1>`)
+//     let firstletter=myAdmins[j][0];
+//     for (let k=0;k<myEmp.length;k++)
+//     {
+//         if(myEmp[k][0]==firstletter){
+//             document.write(`<h3> ${myEmp[k]}</h3>`);
+//         }
+//     }
+//     j++;
+
+// }
+
